d9e9c8a0eca1f2566940b67ac859945b4a4cfd3a
Jose Alberto Moreno Sanchez <jose.moreno@linio.com>
2018-08-18 19:30:18 -0500

    Added styled-component and material-ui.

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,9 @@
 const path = require('path');
+const rewireStyledComponents = require('react-app-rewire-styled-components');
 
 module.exports = function override(config, env) {
+  config = rewireStyledComponents(config, env);
+
   config.resolve.alias = {
     '@app': path.resolve(__dirname, './src/app')
   };
--- src/app/components/App/App.js
+++ src/app/components/App/App.js
@@ -1,7 +1,26 @@
 // @flow
 
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import MenuIcon from '@material-ui/icons/Menu';
+import { StyledMain, StyledIcon, StyledTypography } from './styled';
 
-const App = () => <div>Hola mundo</div>;
+const App = () => (
+  <StyledMain>
+    <AppBar position="static">
+      <Toolbar>
+        <StyledIcon color="inherit" aria-label="Menu">
+          <MenuIcon />
+        </StyledIcon>
+        <StyledTypography variant="title" color="inherit">
+          News
+        </StyledTypography>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+    </AppBar>
+  </StyledMain>
+);
 
 export default App;
